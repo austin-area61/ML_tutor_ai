c1ff5159b04e08bdf0322940b4dcdf856f514463
austin-area61 <austinonyango.area61@gmail.com>
2024-07-19 08:13:28 +0300

    updated the setup.py script

--- ai-tutor/setup.py
+++ ai-tutor/setup.py
@@ -1,21 +1,33 @@
-$content = @"
 from setuptools import setup, find_packages
 
 setup(
-    name='ML_TUTOR_AI',
-    version='0.1.0',
+    name='ai_tutor',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        'pandas',
-        'scikit-learn',
-        'joblib',
+        'pandas==1.3.3',
+        'scikit-learn==0.24.2',
+        'numpy==1.21.2',
     ],
     entry_points={
         'console_scripts': [
-            'preprocess-data=src.utils.data_preprocessing:main',
-            'train-model=models.training_script:main',
+            'data_preprocessing=ai_tutor.src.utils.data_preprocessing:main',
+            'train_model=ai_tutor.models.training_script:main',
         ],
     },
+    package_data={
+        # If any package contains *.txt or *.csv files, include them:
+        '': ['*.txt', '*.csv'],
+    },
+    include_package_data=True,
+    description='AI Tutor Project for analyzing and predicting student performance',
+    author='Austin Onyango',
+    author_email='austinonyango.area61@gmail.com',
+    url='https://github.com/austin-area61/ai_tutor',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
 )
-"@
-Set-Content -Path ai-tutor/setup.py -Value $content
